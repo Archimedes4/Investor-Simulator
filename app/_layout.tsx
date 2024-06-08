bc31bbc0be38d27a0a276c3762860dbf601aa97e
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2024-06-08 11:07:17 -0500

    adding firebase

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,24 @@ import { Slot, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import { initializeApp } from "firebase/app";
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA9nK5jQAs6bzcNFkL1N_um13smK5R2-38",
+  authDomain: "investor-simulator-29aed.firebaseapp.com",
+  projectId: "investor-simulator-29aed",
+  storageBucket: "investor-simulator-29aed.appspot.com",
+  messagingSenderId: "1050022328462",
+  appId: "1:1050022328462:web:20ea8e27515dbd672cf1df",
+  measurementId: "G-TNJ1976RTX"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
--- components/appleSignIn.tsx/index.web.tsx
+++ components/appleSignIn.tsx/index.web.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {} from'react-apple-signin-auth'
 
 export default function AppleSignIn() {
   return (
